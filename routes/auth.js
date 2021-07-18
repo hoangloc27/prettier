a5fe19e37076c476bb53652fab0547b0dc50e150
loc.hoang <loc.hv.270296@gmail.com>
2021-07-18 15:52:49 +0700

    test github actions

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const { signUp, login } = require('../controllers/auth');
 const { verifyToken } = require('../middlewares/authentication');
 const router = app.Router();
 
-router.post('/sign-up', signUp);
+router.post("/sign-up", signUp);
 router.post('/login', login);
 
 module.exports = router;
